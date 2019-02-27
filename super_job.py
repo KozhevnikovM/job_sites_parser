d05f3d111f4cc7e3faa88ccde1b6eeddf95dd9cb
mk <mikhail.kozhevnikov@gmail.com>
2019-02-27 16:06:40 +0300

    replace params with default_params

--- super_job.py
+++ super_job.py
@@ -7,24 +7,23 @@ API_KEY=os.getenv('API_KEY')
 
 
 class SuperJob:
-    def __init__(self, params={}):
+    def __init__(self, default_params={}):
         self.headers = {
             'Host': 'api.superjob.ru',
             'X-Api-App-Id': API_KEY,
             'Authorization': 'Bearer r.000000010000001.example.access_token',
             'Content-Type': 'application/x-www-form-urlencoded'
         }
-        self.params = params
+        self.default_params = default_params
         self.data = requests.get('https://api.superjob.ru/2.0/vacancies/',
                                 headers=self.headers,
-                                params=self.params).json()
+                                params=self.default_params).json()
         self.total = self.data['total']
         self.vacancies = self.data['objects']
 
     def fetch_all_pages(self):
         for page in count(0):
-            params = self.params
-            params['count'] = 100
+            params = {**self.default_params, count:100}
             total_pages = round(self.total / params['count'])
             page_data = self.data
             if page > total_pages:
